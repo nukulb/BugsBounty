8876804480b5788b66d44be70c75cd61136a6c4e
Nukul Bhasin <me@nukulb.com>
2012-02-04 10:20:53 -0500

    Added package.json so you can just call npm install to get all the dependencies. Added express and a couple of useless xhr calls as examples.

---
+++ server.js
@@ -0,0 +1,45 @@
+/*
+//creating a server without express
+ var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(1337, "127.0.0.1");
+console.log('Server running at http://127.0.0.1:1337/');
+*/
+
+//creating a server with express.
+var express = require('express');
+var app = express.createServer();
+
+app.get("/user/:id", function (req,res, next){
+    //lookup in database and send the :id back
+    //let us assume we only support positive natural numbers for id values , reasonable assumption
+    if(req.params.id < 1){
+        res.send('Looked up user ' + req.params.id);
+    } else {
+        //pass the baton to the next app.get so now "lets try /user/* method right below this method.
+        //basically that means that if you did not find an id, it would then go the next method.
+        next();
+    }
+});
+
+app.get("/user/*", function (req, res, next) {
+    res.send("No such user was found.");
+});
+
+app.post('/', function(req, res){
+    res.send(req.body);
+});
+
+//setup a static server
+app.configure(function(){
+    app.use(express.methodOverride());
+    app.use(express.bodyParser());
+    //this is all you have to setup a static server, the public folder is now exposed.
+    app.use(express.static(__dirname + '/public'));
+});
+
+app.listen('3000');
+console.log('Server running at http://127.0.0.1:3000/');
+
