5ea04477bf1b3c9dc2510122b9cdf70c1a7c8911
Rowell Cruz <r@rowellcruz.com>
2012-03-04 14:05:33 -0500

    Added setup instructions for backup

--- db/backupConfig/runBugsBountyBackup.sh
+++ db/backupConfig/runBugsBountyBackup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-/usr/local/bin/automysqlbackup /etc/automysqlbackup/myserver.conf
+/usr/local/bin/automysqlbackup /etc/automysqlbackup/bugsBountyBackup.conf
 
 chown root.root /var/backup/db* -R
 find /var/backup/db* -type f -exec chmod 400 {} \;
