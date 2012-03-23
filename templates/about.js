2a6e319b0fb43c037f33d7388849d0a4a988f761
Nukul Bhasin <me@nukulb.com>
2012-03-23 09:08:31 -0400

    templating the code and maintianing escape chars.

--- templates/about.js
+++ templates/about.js
@@ -1,6 +1,24 @@
-module.exports  = {
+module.exports = {
     locals: {
-        title: "Bugs Bounty"
+        title: "Bugs Bounty",
+        about: {
+            set1: [{
+                heading: "Opportunity",
+                text: "The world mobile application market is expected to grow from $25 billion by 2015. In a crowded market the only way to differentiate your app is to provide top quality with an intuitive user experience. The fragmentaton in the mobile market makes it cumbersome for developers to test their products accross all platforms."
+            },
+            { 
+                heading: "Community",
+                text: "There is a growing community of early adopters that will help developers test their apps and provide invaluable feedback."
+            }],
+            set2: [{
+                heading: "Platform",
+                text: "We will provide online tools and SDKs to help developers deploy their apps to their target audience and get realtime feedback from the community."
+            },
+            { 
+                heading: "Team",
+                text: "We are a bunch of web and mobile developers that feel testing mobile apps in a fragmented and fast evolving market poses unique challenges. We have set a lofty goal for ourselves to make it easy for developers to find testers, deploy apps, collect feedback and iterate on their product. <a href='/'>Sign up</a> here so that we can find the testing solution that best fits your needs."
+            }]
+        }
     },
     partials: {
         otherRole: "<li><a href='/'>Developer?</a></li><li><a href='/tester.html'>Not a Developer?</a></li>"
