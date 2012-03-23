564e40394c66b902864c5794f761e4f14af17d45
Jason Scott <jasscott@rim.com>
2012-03-23 08:16:16 -0400

    Added Navbar button to both tester and non tester pages from about

---
+++ templates/about.js
@@ -0,0 +1,8 @@
+module.exports  = {
+    locals: {
+        title: "Bugs Bounty"
+    },
+    partials: {
+        otherRole: "<li><a href='/'>Developer?</a></li><li><a href='/'>Not a Developer?</a></li>"
+    }
+};
