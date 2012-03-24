edf8feb57c2f2d2126b19543944ccede428559d3
Jason Scott <me@jasondscott.com>
2012-03-24 12:03:36 -0400

    Moving the features copy from the globals to index

--- templates/global.js
+++ templates/global.js
@@ -1,32 +1,6 @@
 module.exports  = {
     locals: {
-        "title": "Bugs Bounty",
-        "features": {
-            "set1": [
-                {
-                    "heading": "Invite testers",
-                    "text": "We know it's tough to find quality testers, let us find them for you."
-                },
-                {
-                    "heading": "Set your bounty",
-                    "text": "Entice your testers with rewards of your choice."
-                },
-                {
-                    "heading": "Distribute your app",
-                    "text": "Deploy your app easily over the air to any platform."
-                }
-            ],
-            "set2": [
-                {
-                    "heading": "Get top quality feedback",
-                    "text": "Receive responses from the community on any aspect of your app."
-                },
-                {
-                    "heading": "Distribute rewards",
-                    "text": "It's important to reward awesome people, show your appreciation."
-                }
-            ]
-        }
+        "title": "Bugs Bounty"
     },
     partials: {
         otherRole: "<li><a href='/tester.html'>Not a Developer?</a></li>",
