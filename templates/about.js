f58d946115c6abff4d19581750c6a88090745ef3
Jason Scott <me@jasondscott.com>
2012-03-25 12:12:39 -0400

    Adjusting copy

--- templates/about.js
+++ templates/about.js
@@ -4,19 +4,19 @@ module.exports = {
         about: {
             set1: [{
                 heading: "Opportunity",
-                text: "The world mobile application market is expected to grow from $25 billion by 2015. In a crowded market the only way to differentiate your app is to provide top quality with an intuitive user experience. The fragmentation in the mobile market makes it cumbersome for developers to test their products across all platforms."
+                text: "The mobile application market is expected to grow to $25 billion by 2015. In a crowded market, the only way to differentiate your app is to provide a top quality app with an intuitive user experience. The fragmentation in the mobile market makes it difficult for developers to test their products across all platforms. Developers need a solution to this growing problem."
             },
             { 
                 heading: "Community",
-                text: "There is a growing community of early adopters that will help developers test their apps and provide invaluable feedback."
+                text: "Our growing community of helpful early adopters will assist developers with app testing. The tester community has access to many mobile platforms and provides developers with valuable feedback."
             }],
             set2: [{
                 heading: "Platform",
-                text: "We will provide online tools and SDKs to help developers deploy their apps to their target audience and get realtime feedback from the community."
+                text: "Our online tools and SDKs will allow developers to deploy their apps to their target audience easily. Get immediate feedback from the community in realtime."
             },
             { 
                 heading: "Team",
-                text: "We are a bunch of web and mobile developers that feel testing mobile apps in a fragmented and fast evolving market poses unique challenges. We have set a lofty goal for ourselves to make it easy for developers to find testers, deploy apps, collect feedback and iterate on their product. <a href='/'>Sign up</a> here so that we can find the testing solution that best fits your needs."
+                text: "We are a group of developers that feel testing mobile apps in a fragmented, fast-evolving market is a difficult and unique challenge. We have taken it upon ourselves to make it easy for all mobile developers to find testers, deploy apps, collect feedback and iterate on their products. Sign up here to find the testing solution that best fits your needs."
             }]
         }
     },
